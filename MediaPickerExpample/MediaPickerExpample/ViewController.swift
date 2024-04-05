a7799127df8ec9d4b14d9833c7d7c1de81503bd8
Valentin <v.panchishen@aspro.ru>
2024-04-05 12:17:48 +0500

    init example project

---
+++ MediaPickerExpample/MediaPickerExpample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaPickerExpample
+//
+//  Created by Валентин Панчишен on 05.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
